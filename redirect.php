b4a2d926e4baaff79613c3479bbc3bd627e3ef01
Pierre Rudloff <contact@rudloff.pro>
2017-09-07 17:08:39 +0000

    Apply fixes from StyleCI

--- redirect.php
+++ redirect.php
@@ -1,7 +1,7 @@
 <?php
 
-use \Psr\Http\Message\ResponseInterface as Response;
-use \Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 error_reporting(E_ALL ^ E_DEPRECATED);
 

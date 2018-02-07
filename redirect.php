c37e5d46dfc1d58b82de332d193ee584f906ab92
Pierre Rudloff <contact@rudloff.pro>
2018-02-07 22:11:22 +0100

    fix: Redirect to an absolute path
    In order to make URLs like /adresse-2_rue_Pierre_Large_Centre_ville_Strasbourg-1230.html/de-de/ work
    correctly.

--- redirect.php
+++ redirect.php
@@ -6,20 +6,25 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 error_reporting(E_ALL ^ E_DEPRECATED);
 
 require_once __DIR__.'/constants.php';
+
+//In order to avoid autoloaded MediaWiki classes complaining
+require_once __DIR__.'/../../mediawiki/core/includes/AutoLoader.php';
+require_once __DIR__.'/../../mediawiki/core/includes/Defines.php';
+require_once __DIR__.'/../../mediawiki/core/includes/DefaultSettings.php';
+require_once __DIR__.'/../../mediawiki/core/includes/GlobalFunctions.php';
+
 if (is_file(__DIR__.'/vendor/autoload.php')) {
     require_once __DIR__.'/vendor/autoload.php';
 } else {
-    //In order to avoid autoloaded MediaWiki classes complaining
-    require_once __DIR__.'/../../mediawiki/core/includes/AutoLoader.php';
-    require_once __DIR__.'/../../mediawiki/core/includes/Defines.php';
-    require_once __DIR__.'/../../mediawiki/core/includes/DefaultSettings.php';
     require_once __DIR__.'/../../autoload.php';
 }
+
+require_once __DIR__.'/../../../LocalSettings.php';
 
 $app = new \Slim\App();
 $app->get('{path:.*}', function (Request $request, Response $response) {
     $params = $request->getQueryParams();
-    global $config;
+    global $config, $wgScript;
     $config = new \ArchiConfig();
     switch ($params['archiAffichage']) {
         case 'adresseDetail':
@@ -52,13 +57,13 @@ $app->get('{path:.*}', function (Request $request, Response $response) {
             $name = str_replace("d' ", "d'", $name);
             $name = trim($name, '.');
 
-            return $response->withRedirect('index.php/Adresse:'.$name, 301);
+            return $response->withRedirect($wgScript.'Adresse:'.$name, 301);
         case 'evenementListe':
             switch ($params['selection']) {
                 case 'personne':
                     $person = new \ArchiPersonne(intval($params['id']));
 
-                    return $response->withRedirect('index.php/Personne:'.$person->prenom.' '.$person->nom, 301);
+                    return $response->withRedirect($wgScript.'Personne:'.$person->prenom.' '.$person->nom, 301);
             }
     }
 });

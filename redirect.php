b7762f85312db7c76b3e3f3498d2b6d8820d28c2
Pierre Rudloff <contact@rudloff.pro>
2018-02-07 22:32:59 +0100

    fix: Don't use local autoloader
    It causes too much trouble.

--- redirect.php
+++ redirect.php
@@ -13,11 +13,7 @@ require_once __DIR__.'/../../mediawiki/core/includes/Defines.php';
 require_once __DIR__.'/../../mediawiki/core/includes/DefaultSettings.php';
 require_once __DIR__.'/../../mediawiki/core/includes/GlobalFunctions.php';
 
-if (is_file(__DIR__.'/vendor/autoload.php')) {
-    require_once __DIR__.'/vendor/autoload.php';
-} else {
-    require_once __DIR__.'/../../autoload.php';
-}
+require_once __DIR__.'/../../autoload.php';
 
 require_once __DIR__.'/../../../LocalSettings.php';
 
